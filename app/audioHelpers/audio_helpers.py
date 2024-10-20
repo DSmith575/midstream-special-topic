4993c63cce0e8e711196cf59424a22368c930f7a
Deacon Smith (1000106517) <SMITDE5@student.op.ac.nz>
2024-10-21 10:29:11 +1300

    made changes

--- app/audioHelpers/audio_helpers.py
+++ app/audioHelpers/audio_helpers.py
@@ -4,17 +4,21 @@ import noisereduce as nr
 import numpy as np
 
 def process_chunks(chunk, min_silence_len, silence_threshold):
-    """Reduce noise and trim silence from an audio chunk."""
-    print("CHUNK", chunk)
-    reduced_chunk = nr.reduce_noise(y=np.array(chunk.get_array_of_samples()), 
-                                    sr=chunk.frame_rate, prop_decrease=0.8)
-    print("REDUCED", reduced_chunk)
-    audio = AudioSegment(reduced_chunk.tobytes(), frame_rate=chunk.frame_rate, 
-                         sample_width=chunk.sample_width, channels=chunk.channels)
-    print("AUDIO", audio)
+    try:
+        """Reduce noise and trim silence from an audio chunk."""
+        print("CHUNK", chunk)
+        reduced_chunk = nr.reduce_noise(y=np.array(chunk.get_array_of_samples()), 
+                                        sr=chunk.frame_rate, prop_decrease=0.8)
+        print("REDUCED", reduced_chunk)
+        audio = AudioSegment(reduced_chunk.tobytes(), frame_rate=chunk.frame_rate, 
+                            sample_width=chunk.sample_width, channels=chunk.channels)
+        print("AUDIO", audio)
 
-    # Trim silence
-    silences = detect_silence(audio, min_silence_len=min_silence_len, 
-                              silence_thresh=silence_threshold)
-    print("SILENCES", silences)
-    return audio[silences[0][0]:silences[-1][1]] if silences else audio
+        # Trim silence
+        silences = detect_silence(audio, min_silence_len=min_silence_len, 
+                                silence_thresh=silence_threshold)
+        print("SILENCES", silences)
+        return audio[silences[0][0]:silences[-1][1]] if silences else audio
+    except Exception as e:
+        print(f"Error processing chunk: {e}")
+        raise
